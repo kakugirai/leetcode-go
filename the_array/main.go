aad2f138d0625ff65fd66956ad518f6416e14a62
Girai Kaku <kakugirai@gmail.com>
2017-10-07 19:54:05 +0900

    add "maps and slice"

--- the_array/main.go
+++ the_array/main.go
@@ -8,4 +8,15 @@ func main() {
 
 	a[4] = 100
 	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
 }
