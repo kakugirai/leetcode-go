af5096166b276d2099e7764b70b0a233cc0f1e03
Girai Kaku <kakugirai@gmail.com>
2017-10-09 14:17:41 +0900

    add from range to pointers

---
+++ closures/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInt := intSeq()
+	fmt.Println(newInt())
+}
