7f31434250ec4e04b8a9c7931039ebbfce2d5a27
Girai Kaku <kakugirai@gmail.com>
2019-02-21 15:28:14 +0800

    hello from windows

--- valid-parentheses/valid-parentheses.go
+++ valid-parentheses/valid-parentheses.go
@@ -1,19 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	fmt.Println(isValid("{[]}{}"))
+type stack []string
+
+func (s stack) Push(v string) stack {
+	return append(s, v)
 }
 
-func isValid(s string) bool {
-	var strs []string
+func (s stack) Pop() string {
+	//l := len(s)
+	return s[0]
+}
+
+func main() {
+	v := "{}["
+	//m := map[string]string{")": "(", "}": "{", "]": "["}
+	s := make(stack, 0)
+	s.Push("a")
+	a := s.Pop()
+	fmt.Println(a)
+	fmt.Println(v[0])
+	fmt.Println(isValid(v))
+}
+
+func isValid(str string) (bool, stack) {
+	// create the stack to keep track of brackets
+	s := make(stack, 0)
+	// create a hash map to keep track of mappings
 	m := map[string]string{")": "(", "}": "{", "]": "["}
-	for _, v := range s {
+	// for every brackets in the expressions
+	for _, v := range str {
+		// if the character is an closing bracket
 		if _, ok := m[string(v)]; ok {
-			if len(strs) != 0:
-				topElement :=
+			var topElement string
+			if len(s) != 0 {
+				topElement = s.Pop()
+			} else {
+				topElement = "#"
+			}
+			if m[string(v)] != topElement {
+				return false, s
+			}
+		} else {
+			s.Push(string(v))
 		}
 	}
-	return false
+	if len(s) == 0 {
+		return true, s
+	} else {
+		return false, s
+	}
 }
