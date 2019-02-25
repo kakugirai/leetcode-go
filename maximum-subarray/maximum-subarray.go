47e3c491a6470411b7a767d9efc9623dbfb9bf11
Girai Kaku <kakugirai@gmail.com>
2019-02-26 01:13:23 +0800

    add maximum subarray

--- maximum-subarray/maximum-subarray.go
+++ maximum-subarray/maximum-subarray.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
@@ -8,5 +10,21 @@ func main() {
 }
 
 func maxSubArray(nums []int) int {
-
+	// Time complexity: O(n)
+	// Space complexity: O(2)
+	// Apparently we can reduce space complexity to O(1) if we choose an in-place solution.
+	// But I found this more readable for me so why don't we just consume more memory :)
+	localMax := nums[0]
+	globalMax := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if localMax <= 0 {
+			localMax = nums[i]
+		} else {
+			localMax += nums[i]
+		}
+		if localMax > globalMax {
+			globalMax = localMax
+		}
+	}
+	return globalMax
 }
