6796e62ac401c00422d42d491a0fbe15ceac629f
Girai Kaku <kakugirai@gmail.com>
2019-05-05 23:25:53 +0900

    add intersection of two arrays

---
+++ intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func intersection(nums1 []int, nums2 []int) []int {
+	var result []int
+	m1 := make(map[int]struct{})
+	m2 := make(map[int]struct{})
+	for _, v := range nums1 {
+		// struct{}{} doesn't occupy any additional space
+		m1[v] = struct{}{}
+	}
+	for _, v := range nums2 {
+		if _, ok := m1[v]; ok {
+			m2[v] = struct{}{}
+		}
+	}
+	for k := range m2 {
+		result = append(result, k)
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(intersection([]int{4, 5, 9}, []int{9, 4, 9, 8, 4}))
+}
--- playground/main.go
+++ playground/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
-
-	defer fmt.Println("main dome")
-	go func() {
-		defer fmt.Println("goroutine1 done")
-	}()
-	fmt.Println("done")
-
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
 }
