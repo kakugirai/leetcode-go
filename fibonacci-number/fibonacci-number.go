fc0a2370220c80b18b2ba08720c3bc7196b4e4e9
Girai Kaku <kakugirai@gmail.com>
2019-05-10 01:27:26 +0900

    modified fibonacci number

--- fibonacci-number/fibonacci-number.go
+++ fibonacci-number/fibonacci-number.go
@@ -2,17 +2,19 @@ package main
 
 import "fmt"
 
-var m map[int]int
-
-func fib(n int) int {
+func helper(n int, m map[int]int) int {
 	m = make(map[int]int, n)
 	m[0], m[1] = 0, 1
 	if _, ok := m[n]; ok {
 		return m[n]
 	}
-	result := fib(n-1) + fib(n-2)
+	result := helper(n-1, m) + helper(n-2, m)
 	m[n] = result
 	return result
+}
+
+func fib(n int) int {
+	return helper(n, map[int]int{})
 }
 
 func main() {
