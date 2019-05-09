f0527307467dcd53a98d91858f62724f071b496c
Girai Kaku <kakugirai@gmail.com>
2019-05-10 00:29:25 +0900

    add find minimum in rotated sorted array ii

---
+++ reverse-linked-list/reverse-linked-list.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		temp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = temp
+	}
+	return prev
+}
+
+func main() {
+	head := ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+	fmt.Println(reverseList(&head))
+}
