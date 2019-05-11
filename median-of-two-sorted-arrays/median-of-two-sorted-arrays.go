c487aaa7700fa1eef10ec195f469a1ff1b97cdb3
Girai Kaku <kakugirai@gmail.com>
2019-05-11 20:18:38 +0900

    add robot return to origin

---
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var result float64
+	return result
+}
+
+func main() {
+	num1 := []int{1, 2}
+	num2 := []int{3, 4}
+	fmt.Println(findMedianSortedArrays(num1, num2))
+}
---
+++ robot-return-to-origin/robot-return-to-origin.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func judgeCircle(moves string) bool {
+	m := map[byte]int{
+		'L': 1,
+		'R': -1,
+		'U': 1,
+		'D': -1,
+	}
+
+	row := 0
+	col := 0
+	for i := range moves {
+		move := moves[i]
+		if move == 'L' || move == 'R' {
+			row += m[move]
+		}
+		if move == 'U' || move == 'D' {
+			col += m[move]
+		}
+	}
+	if row == 0 && col == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	fmt.Println(judgeCircle("LDRRLRUULR"))
+}
