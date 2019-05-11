6904fb5a6d5ade5e430123080e6f191ec6ee5d28
Girai Kaku <kakugirai@gmail.com>
2019-05-11 20:45:14 +0900

    add contain duplicate ii

---
+++ contain-duplicate-ii/contain-duplicate-ii.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	m := make(map[int]int)
+	for i := range nums {
+		if _, ok := m[nums[i]]; ok && i-m[nums[i]] <= k {
+			return true
+		} else {
+			m[nums[i]] = i
+		}
+	}
+	return false
+}
+
+func main() {
+	nums := []int{1, 2, 3, 1, 2, 3}
+	k := 2
+	fmt.Println(containsNearbyDuplicate(nums, k))
+}
