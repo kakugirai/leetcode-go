75ffa15c0bf5a456fdaa1f108c4c59cb76c4f162
Girai Kaku <kakugirai@gmail.com>
2019-05-13 23:58:40 +0900

    add same tree

---
+++ same-tree/same-tree.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	} else if p.Val != q.Val {
+		return false
+	} else {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+}
+
+func main() {
+	t1 := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{4,
+				&TreeNode{7, nil, nil},
+				nil},
+			&TreeNode{5, nil, nil}},
+		&TreeNode{3,
+			&TreeNode{6,
+				&TreeNode{8, nil, nil},
+				&TreeNode{9, nil, nil}},
+			nil}}
+	t2 := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{4,
+				&TreeNode{7, nil, nil},
+				nil},
+			&TreeNode{5, nil, nil}},
+		&TreeNode{3,
+			&TreeNode{6,
+				&TreeNode{8, nil, nil},
+				&TreeNode{9, nil, nil}},
+			nil}}
+	fmt.Println(isSameTree(t1, t2))
+}
