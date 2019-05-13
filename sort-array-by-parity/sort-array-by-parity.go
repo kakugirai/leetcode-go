00756335f3e5602765d1fac1385248dd0856ab88
Girai Kaku <kakugirai@gmail.com>
2019-05-14 01:12:50 +0900

    add sort array by parity

---
+++ sort-array-by-parity/sort-array-by-parity.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sortArrayByParity(A []int) []int {
+	result := make([]int, len(A))
+	i := 0
+	j := len(A) - 1
+	for k := range A {
+		if A[k]%2 == 0 {
+			result[i] = A[k]
+			i++
+		} else if A[k]%2 != 0 {
+			result[j] = A[k]
+			j--
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(sortArrayByParity([]int{3, 1, 2, 4}))
+}
