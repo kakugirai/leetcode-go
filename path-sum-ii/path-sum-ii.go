9f5afca6f988e78fd7ca4e44cf7508b5dec85305
Girai Kaku <kakugirai@gmail.com>
2019-05-15 17:37:46 +0900

    add path sum ii

---
+++ path-sum-ii/path-sum-ii.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) [][]int {
+	var paths [][]int
+	var current []int
+	findPaths(root, sum, current, &paths)
+	return paths
+}
+
+func findPaths(root *TreeNode, sum int, current []int, paths *[][]int) {
+	if root == nil {
+		return
+	}
+	current = append(current, root.Val)
+	if root.Val == sum && root.Left == nil && root.Right == nil {
+		*paths = append(*paths, current)
+		return
+	}
+	temp := make([]int, len(current))
+	copy(temp, current)
+	findPaths(root.Left, sum-root.Val, temp, paths)
+	findPaths(root.Right, sum-root.Val, temp, paths)
+}
+
+func main() {
+	tree := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{4,
+				&TreeNode{7, nil, nil},
+				nil},
+			&TreeNode{5, nil, nil}},
+		&TreeNode{3,
+			&TreeNode{6,
+				&TreeNode{8, nil, nil},
+				&TreeNode{9, nil, nil}},
+			nil}}
+	fmt.Println(pathSum(tree, 14))
+}
