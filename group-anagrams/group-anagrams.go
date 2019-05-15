c29296fa881ac4f38b731aa24283e31dedf59ac5
Girai Kaku <kakugirai@gmail.com>
2019-05-15 16:15:48 +0900

    add group anagrams

---
+++ group-anagrams/group-anagrams.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	var result [][]string
+	m := make(map[string][]string)
+	for _, current := range strs {
+		var c []byte
+		for i := range current {
+			c = append(c, current[i])
+		}
+		sort.Slice(c, func(i int, j int) bool { return c[i] < c[j] })
+		sorted := string(c)
+		if _, ok := m[sorted]; !ok {
+			m[sorted] = []string{}
+		}
+		m[sorted] = append(m[sorted], current)
+	}
+
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
+
+func main() {
+	strs := []string{
+		"eat", "tea", "tan", "ate", "nat", "bat",
+	}
+	fmt.Println(groupAnagrams(strs))
+}
