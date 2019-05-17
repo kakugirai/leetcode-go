e5e6cb06223aff9c9bb066693a7a4a77af3d6e5e
Girai Kaku <kakugirai@gmail.com>
2019-05-17 22:33:55 +0900

    add longest palindrome

---
+++ longest-palindrome/longest-palindrome.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func longestPalindrome(s string) int {
+	var result int
+	m := make(map[byte]int)
+	for i := range s {
+		m[s[i]]++
+	}
+
+	haveOddNumber := false
+	for _, v := range m {
+		if v%2 == 0 {
+			result += v
+		}
+		if v%2 == 1 {
+			haveOddNumber = true
+			result += (v / 2) * 2
+		}
+	}
+	if haveOddNumber {
+		return result + 1
+	} else {
+		return result
+	}
+}
+
+func main() {
+	s := "abbbccccdd"
+	fmt.Println(longestPalindrome(s))
+}
