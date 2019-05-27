afc9f6b4572fa9b6f5f2b863bcee556a978e3b8d
Girai Kaku <kakugirai@gmail.com>
2019-05-27 23:24:38 +0900

    add two numbers

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ My LeetCode solutions written in Go.
 | Question ID | Question | Difficulty | Solution |
 | :---------: | -------- | :--------: | :------: |
 |1|[Two Sum](https://leetcode.com/problems/two-sum)|Easy|[Solution](src/two-sum)|
+|2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers)|Medium|[Solution](src/add-two-numbers)|
 |4|[Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays)|Hard|[Solution](src/median-of-two-sorted-arrays)|
 |7|[Reverse Integer](https://leetcode.com/problems/reverse-integer)|Easy|[Solution](src/reverse-integer)|
 |9|[Palindrome Number](https://leetcode.com/problems/palindrome-number)|Easy|[Solution](src/palindrome-number)|
---
+++ src/add-two-numbers/add-two-numbers.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+//ListNode is a singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	c1 := l1
+	c2 := l2
+	var result = new(ListNode)
+	root := result
+	sum := 0
+	for c1 != nil || c2 != nil {
+		sum /= 10
+		if c1 != nil {
+			sum += c1.Val
+			c1 = c1.Next
+		}
+		if c2 != nil {
+			sum += c2.Val
+			c2 = c2.Next
+		}
+		root.Next = &ListNode{sum % 10, nil}
+		root = root.Next
+	}
+
+	if sum/10 == 1 {
+		root.Next = &ListNode{1, nil}
+	}
+	return result.Next
+}
+
+func main() {
+	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+	fmt.Println(addTwoNumbers(l1, l2))
+}
