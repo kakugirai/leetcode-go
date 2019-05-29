d601c3f099754575634383f57586607e5aab7ae8
Girai Kaku <kakugirai@gmail.com>
2019-05-29 22:33:37 +0900

    add delete node in a linked list

---
+++ src/delete-node-in-a-linked-list/delete-node-in-a-linked-list.go
@@ -0,0 +1,20 @@
+package delete_node_in_a_linked_list
+
+import "fmt"
+
+//ListNode is a singly-linked list.
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+
+func main() {
+    node := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+    deleteNode(node)
+    fmt.Println(node)
+}
