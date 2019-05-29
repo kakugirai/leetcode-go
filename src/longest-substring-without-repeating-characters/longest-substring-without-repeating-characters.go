66741110f757aa3a03e060a82fec623d5e450d72
Girai Kaku <kakugirai@gmail.com>
2019-05-29 12:36:24 +0900

    optimize longest substring without repeating characters

--- src/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ src/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,42 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
 func lengthOfLongestSubstring(s string) int {
-	var ss []string
-
-	m := make(map[byte]struct{})
+	var result int
 	var sb []byte
+	m := make(map[byte]struct{})
 	for i := range s {
 		if _, ok := m[s[i]]; !ok {
 			m[s[i]] = struct{}{}
 			sb = append(sb, s[i])
 		} else {
-			ss = append(ss, string(sb))
-			for j := range sb {
-				if sb[j] == s[i] {
-					for k := range sb[:j] {
-						delete(m, sb[k])
-					}
-					sb = sb[j+1:]
-					break
-				}
+			if len(sb) > result {
+				result = len(sb)
 			}
+			idx := bytes.IndexByte(sb, s[i])
+			for k := 0; k < idx; k++ {
+				delete(m, sb[k])
+			}
+			sb = sb[idx+1:]
 			sb = append(sb, s[i])
 		}
 	}
-	ss = append(ss, string(sb))
-
-	var current string
-	for i := range ss {
-		if len(ss[i]) > len(current) {
-			current = ss[i]
-		}
+	if len(sb) > result {
+		result = len(sb)
 	}
-
-	return len(current)
+	return result
 }
 
 func main() {
